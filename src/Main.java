63218c610e50656e7b9013e0a764eb9b47c30bd5
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-14 03:15:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,80 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Исключение, выбрасываемое при некорректном размере массива
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+// Исключение, выбрасываемое при некорректных данных в массиве
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(int row, int col) {
+        super(String.format("Invalid data at cell [%d,%d]", row, col));
+    }
+}
+
+public class Main {
+    // Метод для обработки двумерного массива
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Array must be 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        String[][] incorrectSizeArray = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+
+        String[][] incorrectDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "X", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            // Корректный массив
+            int result = processArray(correctArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            // Массив неправильного размера
+            int result = processArray(incorrectSizeArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            // Массив с некорректными данными
+            int result = processArray(incorrectDataArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
